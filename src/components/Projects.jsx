cac31fa5cc9c9d5ffaa82ee67181e7e34930d325
Ashish <145586793+ashishpathak1470@users.noreply.github.com>
2024-07-20 14:15:54 +0530

    ch

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,9 @@ function Projects() {
   return (
     <div id="projects" className="text-cyan-500 p-4 min-h-screen">
       <div className="text-center">
-        <p className="text-3xl sm:text-4xl md:text-5xl font-bold relative inline-block">Projects</p>
+        <p className="text-3xl sm:text-4xl md:text-5xl font-bold relative inline-block">
+          Projects
+        </p>
         <hr className="w-3/4 sm:w-1/2 mx-auto border-t-2 border-cyan-500 mb-8 mt-3" />
       </div>
       <div className="mt-8 grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
@@ -12,17 +14,30 @@ function Projects() {
           <h2 className="text-2xl sm:text-3xl font-bold mb-2">CRM App</h2>
           <hr className="border-t-2 border-cyan-400 mb-4" />
           <p className="mb-4">
-            Transforming how you manage client relationships, CRM App brings together effortless client management, lightning-fast real-time communication, intuitive task tracking, customizable dashboards, powerful analytics for smart decisions, and robust security in one user-friendly platform. Revolutionize your business efficiency with CRM App and stay ahead in client management.
+            Transforming how you manage client relationships, CRM App brings
+            together effortless client management, lightning-fast real-time
+            communication, intuitive task tracking, customizable dashboards,
+            powerful analytics for smart decisions, and robust security in one
+            user-friendly platform. Revolutionize your business efficiency with
+            CRM App and stay ahead in client management.
           </p>
           <div className="flex flex-row justify-center items-center">
-            <a href="https://github.com/ashishpathak1470/APP-CRM" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://github.com/ashishpathak1470/APP-CRM"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img
                 className="mx-4 w-7 sm:w-8 md:w-9 h-7 sm:h-8 md:h-9 hover:scale-110 shadow-2xl"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/1200px-Git_icon.svg.png"
                 alt="GitHub"
               />
             </a>
-            <a href="https://app-crm-beta.vercel.app/" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://app-crm-beta.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img
                 className="mx-4 w-7 sm:w-8 md:w-9 h-7 sm:h-8 md:h-9 hover:scale-110 shadow-2xl"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/OOjs_UI_icon_external-link-ltr-progressive.svg/640px-OOjs_UI_icon_external-link-ltr-progressive.svg.png"
@@ -35,17 +50,31 @@ function Projects() {
           <h2 className="text-2xl sm:text-3xl font-bold mb-2">Movie Library</h2>
           <hr className="border-t-2 border-cyan-400 mb-4" />
           <p className="mb-4">
-            Dive into a world of entertainment with my comprehensive movie library app. Discover and organize your favorite movies effortlessly, explore new releases, and create personalized watchlists. With sleek design and user-friendly features, Movie Library is your ultimate companion for discovering, organizing, and enjoying movies. Start exploring today and unlock a world of cinematic wonders at your fingertips.
+            Dive into a world of entertainment with my comprehensive movie
+            library app. Discover and organize your favorite movies
+            effortlessly, explore new releases, and create personalized
+            watchlists. With sleek design and user-friendly features, Movie
+            Library is your ultimate companion for discovering, organizing, and
+            enjoying movies. Start exploring today and unlock a world of
+            cinematic wonders at your fingertips.
           </p>
           <div className="flex flex-row justify-center items-center">
-            <a href="https://github.com/ashishpathak1470/Movie-Library" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://github.com/ashishpathak1470/Movie-Library"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img
                 className="mx-4 w-7 sm:w-8 md:w-9 h-7 sm:h-8 md:h-9 hover:scale-110 shadow-2xl"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/1200px-Git_icon.svg.png"
                 alt="GitHub"
               />
             </a>
-            <a href="https://movie-library-bay.vercel.app/" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://movie-library-bay.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img
                 className="mx-4 w-7 sm:w-8 md:w-9 h-7 sm:h-8 md:h-9 hover:scale-110 shadow-2xl"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/OOjs_UI_icon_external-link-ltr-progressive.svg/640px-OOjs_UI_icon_external-link-ltr-progressive.svg.png"
@@ -58,17 +87,28 @@ function Projects() {
           <h2 className="text-2xl sm:text-3xl font-bold mb-2">Seamless Rail</h2>
           <hr className="border-t-2 border-cyan-400 mb-4" />
           <p className="mb-4">
-            Experience effortless booking with my innovative rail reservation app. Seamlessly allocate seats for families and friends, ensuring everyone can sit together throughout the journey. Discover convenience with user-friendly features and real-time seat availability updates. Simplify your travel plans with Seamless Rail and enjoy a journey where togetherness meets comfort.
+            Experience effortless booking with my innovative rail reservation
+            app. Seamlessly allocate seats for families and friends, ensuring
+            everyone can sit together throughout the journey. Discover
+            convenience with user-friendly features and real-time seat
+            availability updates. Simplify your travel plans with Seamless Rail
+            and enjoy a journey where togetherness meets comfort.
           </p>
           <div className="flex flex-row justify-center items-center">
-            <a href="https://github.com/ashishpathak1470/Seamless-Rail" rel="noopener noreferrer">
+            <a
+              href="https://github.com/ashishpathak1470/Seamless-Rail"
+              rel="noopener noreferrer"
+            >
               <img
                 className="mx-4 w-7 sm:w-8 md:w-9 h-7 sm:h-8 md:h-9 hover:scale-110 shadow-2xl"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/1200px-Git_icon.svg.png"
                 alt="GitHub"
               />
             </a>
-            <a href="https://github.com/ashishpathak1470/Seamless-Rail" rel="noopener noreferrer">
+            <a
+              href="https://github.com/ashishpathak1470/Seamless-Rail"
+              rel="noopener noreferrer"
+            >
               <img
                 className="mx-4 w-7 sm:w-8 md:w-9 h-7 sm:h-8 md:h-9 hover:scale-110 shadow-2xl"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/OOjs_UI_icon_external-link-ltr-progressive.svg/640px-OOjs_UI_icon_external-link-ltr-progressive.svg.png"
@@ -81,17 +121,30 @@ function Projects() {
           <h2 className="text-2xl sm:text-3xl font-bold mb-2">Quick Entity</h2>
           <hr className="border-t-2 border-cyan-400 mb-4" />
           <p className="mb-4">
-            Simplify entity management with my efficient Quick Entity app. Quickly create entities and store data effortlessly. Whether you're managing projects, clients, or any other data, Quick Entity streamlines the process with intuitive features and robust data storage capabilities. Start organizing your entities with ease and speed using Quick Entity today.
+            Simplify entity management with my efficient Quick Entity app.
+            Quickly create entities and store data effortlessly. Whether you're
+            managing projects, clients, or any other data, Quick Entity
+            streamlines the process with intuitive features and robust data
+            storage capabilities. Start organizing your entities with ease and
+            speed using Quick Entity today.
           </p>
           <div className="flex flex-row justify-center items-center">
-            <a href="https://github.com/ashishpathak1470/CRUD-App" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://github.com/ashishpathak1470/CRUD-App"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img
                 className="mx-4 w-7 sm:w-8 md:w-9 h-7 sm:h-8 md:h-9 hover:scale-110 shadow-2xl"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/1200px-Git_icon.svg.png"
                 alt="GitHub"
               />
             </a>
-            <a href="https://github.com/ashishpathak1470/CRUD-App" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://github.com/ashishpathak1470/CRUD-App"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img
                 className="mx-4 w-7 sm:w-8 md:w-9 h-7 sm:h-8 md:h-9 hover:scale-110 shadow-2xl"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/OOjs_UI_icon_external-link-ltr-progressive.svg/640px-OOjs_UI_icon_external-link-ltr-progressive.svg.png"
